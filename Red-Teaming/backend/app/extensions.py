e474b647284f5d1a9ff6e22fd7d7f5a6828952af
manar <mnarzaid247@gmail.com>
2026-06-18 16:15:49 +0300

    update the database

--- Red-Teaming/backend/app/extensions.py
+++ Red-Teaming/backend/app/extensions.py
@@ -1,11 +1,23 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./red_teaming.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./red_teaming.db"
+)
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
