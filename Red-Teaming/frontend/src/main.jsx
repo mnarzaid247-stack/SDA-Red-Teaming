0fecfd5dc51cf61089e24b5d1ca935d3afb867dd
LujainYaser1 <lujainyaaser@gmail.com>
2026-06-24 11:49:03 +0300

    Update

--- Red-Teaming/frontend/src/main.jsx
+++ Red-Teaming/frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './AuthFolder/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
+    </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
